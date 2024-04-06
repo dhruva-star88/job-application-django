5da85deb441992a96061c587a88db612d3ad7090
dhruva-star88 <dhruvatej6565@gmail.com>
2024-04-06 23:09:49 +0530

    mild change

--- job_application/views.py
+++ job_application/views.py
@@ -9,8 +9,8 @@ def index(request):
     if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
-            first_name = form.cleaned_data["fname"]
-            last_name = form.cleaned_data["lname"]
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
             email = form.cleaned_data["email"]
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
